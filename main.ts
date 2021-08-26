60b6ef5040042dfb3cb8ed5c9b69b2fa63dd9fb0
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-08-26 10:18:54 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
+let Niveau = 0
 let Vibrations = 0
 radio.setGroup(1)
 basic.forever(function () {
     Vibrations = input.acceleration(Dimension.Strength) - 1023
-    radio.sendValue("Vibrations", Vibrations)
+    Niveau = Vibrations * 100 / 1023
+    radio.sendValue("Vibrations", Niveau)
     led.plotBarGraph(
-    Vibrations,
-    0
+    Niveau,
+    100
     )
 })
